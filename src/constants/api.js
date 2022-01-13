5efab87e6d47134732b855d32c9364671b45e92d
Yulia Alenkova <yulia.alenkova@mera.com>
2022-01-14 01:29:40 +0300

    yulalenk

--- src/constants/api.js
+++ src/constants/api.js
@@ -6,6 +6,6 @@ export const SHOW_LIST_REQUEST = `${process.env.REACT_APP_APISERVER}/list`;
 export const SHOW_QUESTIONS_REQUEST = `${process.env.REACT_APP_APISERVER}/questions`;
 export const EMAILS_REQUEST = `${process.env.REACT_APP_APISERVER}/email`;
 
-export const START_CHAT_REQUEST = `http://localhost:8080/users/ws`;
-export const ADD_USER_CHAT_REQUEST = `http://localhost:8080/app/chat.addUser`;
-export const SEND_MESSAGE_CHAT_REQUEST = `http://localhost:8080/app/chat.sendMessage`;
+export const START_CHAT_REQUEST = `http://summerschoolspring-env-1.eba-yprtczhe.eu-west-3.elasticbeanstalk.com/users/ws`;
+export const ADD_USER_CHAT_REQUEST = `http://summerschoolspring-env-1.eba-yprtczhe.eu-west-3.elasticbeanstalk.com/app/chat.addUser`;
+export const SEND_MESSAGE_CHAT_REQUEST = `http://summerschoolspring-env-1.eba-yprtczhe.eu-west-3.elasticbeanstalk.com/app/chat.sendMessage`;
